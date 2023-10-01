256aaab8bedff8c7a24cbc98940973f119179d7c
Scott Benjamin <scott.benjamin@mantiumai.com>
2023-09-30 20:35:40 -0600

    sonoma update

--- config/nvim-new/init.lua
+++ config/nvim-new/init.lua
@@ -177,6 +177,7 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
+    main= "ibl",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     opts = {
