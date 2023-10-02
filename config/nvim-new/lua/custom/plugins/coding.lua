be515d18b9997a1338761ffe6d6cb5c0b7997ba8
Scott Benjamin <scott.benjamin@gmail.com>
2023-10-02 21:02:54 +0000

    latest zellij

--- config/nvim-new/lua/custom/plugins/coding.lua
+++ config/nvim-new/lua/custom/plugins/coding.lua
@@ -1,21 +1,26 @@
 return {
-	-- Conform, to replace null-ls
-	{
-		"stevearc/conform.nvim",
-		opts = {
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-			formatters_by_ft = {
-				lua = { "stylua" },
-				-- Conform will run multiple formatters sequentially
-				python = { "ruff_fix", "ruff_format", "isort", "black" },
-				-- Use a sub-list to run only the first available formatter
-				javascript = { { "prettierd", "prettier" } },
-				["_"] = { "trim_whitespace", "trim_newlines" },
-			},
-		},
-	},
+  -- Conform, to replace null-ls
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform will run multiple formatters sequentially
+        python = { 'ruff_fix', 'ruff_format', 'isort', 'black' },
+        -- Use a sub-list to run only the first available formatter
+        javascript = { { 'prettierd', 'prettier' } },
+        ['_'] = { 'trim_whitespace', 'trim_newlines' },
+      },
+    },
+  },
+
+  {
+    'IndianBoy42/tree-sitter-just',
+    opts = {},
+  },
 }
