89a8758f6259b0ad6e183bc51347fdf5a51f1751
Scott Benjamin <scott.benjamin@mantiumai.com>
2023-11-15 09:05:39 -0700

    daily update

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>gm", ":!glab mr view --web<cr><cr>", { desc = "View current MR" })
-vim.keymap.set("n", "<leader>gM", ":!glab mr new --web --fill<cr><cr>", { desc = "Create New MR" })
+vim.keymap.set("n", "<leader>gm", ":Octo pr browser<cr>", { desc = "View current PR" })
+vim.keymap.set("n", "<leader>gM", ":Octo pr create<cr>", { desc = "Create New PR" })
--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -38,4 +38,13 @@ return {
       },
     },
   },
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+      { "nvim-tree/nvim-web-devicons" },
+    },
+    keys = {},
+  },
 }
