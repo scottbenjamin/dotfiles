211a59de0723cb3622e25feb3e465370450b7959
Scott Benjamin <scott.benjamin@gmail.com>
2024-03-04 04:09:27 +0000

    adding obsidian plugin

--- config/nvim-new/init.lua
+++ config/nvim-new/init.lua
@@ -808,13 +808,13 @@ require('lazy').setup {
   --
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
-
+  --
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
