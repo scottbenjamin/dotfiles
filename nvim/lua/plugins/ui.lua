395ac20cf2f6f27c25e9f741b378915de43c6cc9
Scott Benjamin <sbenjamin@absci.com>
2024-08-30 17:41:15 -0700

    latest

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,11 +1,15 @@
 return {
+
   -- tokyonight
+  -- Colorscheme
   {
     "folke/tokyonight.nvim",
     lazy = true,
     opts = { style = "night" },
   },
+
   -- oil
+  -- file manager
   {
     "stevearc/oil.nvim",
     opts = {},
@@ -21,6 +25,7 @@ return {
   { "AckslD/swenv.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
 
   -- Conform
+  -- Syntax Formatter
   {
     "stevearc/conform.nvim",
     opts = {
@@ -33,10 +38,23 @@ return {
 
   -- Quicker.nvim
   -- https://github.com/stevearc/quicker.nvim
+  -- Better quickfix
   {
     "stevearc/quicker.nvim",
     ---@module "quicker"
     ---@type quicker.SetupOptions
     opts = {},
+  },
+
+  -- Arrow
+  -- https://github.com/otavioschwanck/arrow.nvim
+  -- like harbpoon
+  {
+    "otavioschwanck/arrow.nvim",
+    opts = {
+      show_icons = true,
+      leader_key = ";", -- Recommended to be a single key
+      buffer_leader_key = "m", -- Per Buffer Mappings
+    },
   },
 }
