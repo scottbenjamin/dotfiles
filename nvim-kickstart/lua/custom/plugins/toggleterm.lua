1c04e01149d2e7e7caac02f858ca458f98b71e49
Scott Benjamin <sbenjamin@absci.com>
2024-11-08 18:18:08 -0800

    remove old nvim configs and a gitignore change

--- nvim-kickstart/lua/custom/plugins/toggleterm.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    'akinsho/toggleterm.nvim',
-    version = '*',
-    config = true,
-    opts = {},
-  },
-}
--- nvim.lazy/init.lua
+++
@@ -1,3 +0,0 @@
-vim.g.python3_host_prog = "~/.pyenv/versions/neovim3/bin/python"
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
