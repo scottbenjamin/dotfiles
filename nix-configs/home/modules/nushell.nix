3a6377b54d8dbc7219073df341a1a9115c9a5e33
Scott Benjamin <sbenjamin55@proton.me>
2024-11-12 01:35:29 +0000

    nushell commands

--- nix-configs/home/modules/nushell.nix
+++ nix-configs/home/modules/nushell.nix
@@ -37,29 +37,32 @@
           nvim
         }
 
-	def _get_nix_rebuild_cmd [] {
-	    if $env:OS == 'Darwin' {
-		return "darwin-rebuild"
-	    } else if $env:OS == 'Linux' {
-		return "nix build"
-	    } else {
-		return "Unknown OS"
-	    }	
-	}
 
+        # nix/darwin-build Tools
+        # Rebuild and switch
+        def drs [] {
+          darwin-build switch --flake $env.MY_NIX_CONFIGS;
+        }
 
-        # nix/darwin-build
-	# Rebuild and switch
-	def nrs [] {
-	  let $cmd = _get_nix_rebuild_cmd()
-	  ($cmd) switch --flake $env.MY_NIX_CONFIGS;
-	}
+        # Check the flake
+        def drc [] {
+          let $cmd = _get_nix_rebuild_cmd()
+          darwin-build check --flake $env.MY_NIX_CONFIGS;
+        }
 
-	# Check the flake
-	def nrc [] {
-	  let $cmd = _get_nix_rebuild_cmd()
-	  ($cmd) check --flake $env.MY_NIX_CONFIGS;
-	}
+        def _get_nix_rebuild_cmd [] {
+          if (uname | get operating-system) == 'Darwin' {
+            return "darwin-rebuild"
+          } else if (uname | get operating-system|str contains Linux) {
+            return "nix build"
+          } else {
+            return "Unknown OS"
+          }	
+        }
+
+        def hms [] {
+          home-manager switch --flake $env.MY_NIX_CONFIGS;
+        }
       '';
 
       # Extra things to add to the env.nu file
@@ -71,7 +74,7 @@
         path add  /nix/var/nix/profiles/default/bin
         path add  /run/current-system/sw/bin
         path add  /usr/local/bin
-	$env.MY_NIX_CONFIGS = "~/code/dotfiles/nix-configs"
+        $env.MY_NIX_CONFIGS = "~/code/dotfiles/nix-configs"
       '';
 
       shellAliases = {
