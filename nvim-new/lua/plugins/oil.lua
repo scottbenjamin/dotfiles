ebdbe926c5e9e8c028e0b2a7ecb9ba052b9d70d9
Scott Benjamin <sbenjamin@absci.com>
2025-02-04 20:09:49 -0800

    cleanup

--- nvim-new/lua/plugins/oil.lua
+++ nvim-new/lua/plugins/oil.lua
@@ -3,11 +3,9 @@ return {
     "stevearc/oil.nvim",
     enabled = false,
     event = "VeryLazy",
-    ---@module 'oil'
-    ---@type oil.SetupOpts
     opts = {},
     -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     lazy = false,
   },
 }
