31ab605c19e73da5255e33558ad73266c4d48cbe
Scott Benjamin <sbenjamin@absci.com>
2025-02-04 19:55:20 -0800

    Updating deps for oil

--- nvim-new/lua/plugins/oil.lua
+++ nvim-new/lua/plugins/oil.lua
@@ -6,9 +6,7 @@ return {
     ---@type oil.SetupOpts
     opts = {},
     -- Optional dependencies
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     lazy = false,
   },
 }
