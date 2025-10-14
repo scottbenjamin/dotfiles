b1d72af52d3ca4c465f058f2f80d2f415760a75e
Scott Benjamin <scott.benjamin@gmail.com>
2025-10-13 22:00:45 -0700

    hyprland updates

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,12 @@
 fish_add_path ~/.local/bin
 fish_add_path ~/.npm-packages/bin
 fish_add_path /opt/homebrew/bin
-fish_add_path /etc/profiles/per-user/$USER/bin/
 
 for p in /run/current-system/sw/bin ~/bin
     if not contains $p $fish_user_paths
         set -g fish_user_paths $p $fish_user_paths
     end
 end
-
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -17,48 +15,48 @@ if status is-interactive
     set -xg VISUAL_EDITOR nvim
 
     if type -q brew
-      if test -d (brew --prefix)"/share/fish/completions"
-          set -p fish_complete_path (brew --prefix)/share/fish/completions
-      end
+        if test -d (brew --prefix)"/share/fish/completions"
+            set -p fish_complete_path (brew --prefix)/share/fish/completions
+        end
 
-      if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-          set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
-      end
+        if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+            set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+        end
 
-      if test -d (brew --prefix rustup )"/bin"
-        fish_add_path  (brew --prefix rustup )"/bin"
-      end
+        if test -d (brew --prefix rustup )"/bin"
+            fish_add_path (brew --prefix rustup )"/bin"
+        end
 
     end
 
     if type -q pyenv
-      pyenv init - fish | source
+        pyenv init - fish | source
     end
 
     if type -q direnv
-      direnv hook fish | source
+        direnv hook fish | source
     end
 
-    if type -q carapace 
-      # ~/.config/fish/config.fish
-      set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
-      carapace _carapace | source
+    if type -q carapace
+        # ~/.config/fish/config.fish
+        set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
+        carapace _carapace | source
     end
 
     if type -q atuin
-      atuin init fish | source
+        atuin init fish | source
     end
 
     if type -q zoxide
-      zoxide init fish | source
+        zoxide init fish | source
     end
 
     if type -q starship
-      starship init fish | source
+        starship init fish | source
     end
 
     if test -f ~/.local.fish
-      source ~/.local.fish
+        source ~/.local.fish
     end
 
     function bind_bang
@@ -84,5 +82,10 @@ if status is-interactive
         bind ! bind_bang
         bind '$' bind_dollar
     end
-end
 
+    if test -d /opt/wine-cachyos
+        set -gx PATH /opt/wine-cachyos/bin $PATH
+        set -gx WINEDLLPATH /opt/wine-cachyos/lib/wine /opt/wine-cachyos/lib32/wine $WINEDLLPATH
+        set -gx LD_LIBRARY_PATH /opt/wine-cachyos/lib /opt/wine-cachyos/lib32 $LD_LIBRARY_PATH
+    end
+end
