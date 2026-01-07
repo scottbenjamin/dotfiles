5847b8d8cf91e1471ad91fab1e2dc3c52e0e3fab
Scott Benjamin <sbenjamin@absci.com>
2026-01-07 10:49:24 -0800

    more  fish tuning

--- fish/conf.d/00-env.fish
+++ fish/conf.d/00-env.fish
@@ -2,5 +2,8 @@
 # Disable mise auto-activate - we use shims instead
 set -gx MISE_FISH_AUTO_ACTIVATE 0
 
+# Disable direnv auto-hook - we lazy load it instead
+set -gx DIRENV_DISABLE_HOOK 1
+
 # Cache brew prefix
 set -gx HOMEBREW_PREFIX /opt/homebrew
