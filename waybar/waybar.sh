a6f5d4a9178805ae827d925932e6cf57aaf601c6
Scott Benjamin <scott.benjamin@gmail.com>
2026-03-25 23:19:07 -0700

    stuff

--- waybar/waybar.sh
+++ waybar/waybar.sh
@@ -6,5 +6,13 @@ killall -q waybar
 # Wait until the processes have been shut down
 while pgrep -x waybar >/dev/null; do sleep 1; done
 
-# Launch main
-waybar &
+style="$HOME/.config/waybar/style.css"
+if [ -n "${NIRI_SOCKET:-}" ] && [ -f "$HOME/.config/waybar/config-niri.jsonc" ]; then
+  config="$HOME/.config/waybar/config-niri.jsonc"
+elif [ -n "${HYPRLAND_INSTANCE_SIGNATURE:-}" ] && [ -f "$HOME/.config/waybar/config-hyprland.jsonc" ]; then
+  config="$HOME/.config/waybar/config-hyprland.jsonc"
+else
+  config="$HOME/.config/waybar/config.jsonc"
+fi
+
+waybar -c "$config" -s "$style" &
