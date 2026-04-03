c427c20766ba78b67d7c2e091a71e0167ab5df68
Scott Benjamin <sbenjamin@absci.com>
2026-04-03 13:25:38 -0700

    tuning

--- nvim.old/lua/plugins/lsp.lua
+++ nvim.old/lua/plugins/lsp.lua
@@ -3,6 +3,22 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      setup = {
+        terraformls = function()
+          -- Disable semantic tokens: terraform-ls returns invalid deltaStart
+          -- values for heredoc interpolations, causing an infinite loop in
+          -- nvim 0.12's semantic_tokens handler (neovim/neovim#36257)
+          vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(args)
+              local client = vim.lsp.get_client_by_id(args.data.client_id)
+              if client and client.name == "terraformls" then
+                client.server_capabilities.semanticTokensProvider = nil
+              end
+            end,
+          })
+          return false -- let LazyVim continue normal server setup
+        end,
+      },
       servers = {
         nil_ls = {
           mason = false,
